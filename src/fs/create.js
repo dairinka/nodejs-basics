b9ffc6d180a341319236d1cce5be036f008b9e33
Iryna Yehorova <interest4me@gmail.com>
2024-01-25 00:26:13 +0200

    feat: add implement fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,10 @@
+import { writeFile } from 'node:fs/promises';
 const create = async () => {
-
+    const data = "I am fresh and young";
+    const file = "fresh.txt";
+    writeFile(file, data, {flag: 'wx'})
+    .then(() => console.log(` => file ${file} was created`))
+    .catch(() => console.log('FS operation failed'))
 };
 
 await create();
