7f6e6fe88737afa3a9ff37cd820376525f637dc9
Iryna Yehorova <interest4me@gmail.com>
2024-01-26 20:35:36 +0200

    feat: add solution to stream read file

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { pipeline } from 'node:stream/promises';
+import { stdout } from "node:process";
+import { createReadStream } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const fileToRead = join(__dirname, 'files/fileToRead.txt');
+   
+    try {
+        await pipeline(createReadStream(fileToRead), stdout);
+     } catch {
+        throw Error("Something went wrong");
+    }
+    
 };
 
 await read();
