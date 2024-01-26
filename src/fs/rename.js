1de2d0d7c5cf52cc698587ba40cd04dde6d170ec
Iryna Yehorova <interest4me@gmail.com>
2024-01-26 16:44:09 +0200

    feat: add solution to rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import { rename as rn, access }  from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const srcFolder = join(__dirname, 'files/');
+    const newFile = join(srcFolder, 'properFilename.md');
+    const oldFile = join(srcFolder, 'wrongFilename.txt');
+
+    try{
+        await access(oldFile);
+    } catch {
+        throw new Error('FS operation failed');
+    }
+
+    try{
+        await access(newFile);
+        throw new Error('exist');
+    } catch(err) {
+        if(err.message === 'exist') throw new Error('FS operation failed');
+        try{
+            await rn(oldFile, newFile);
+            console.log('File was successfuly coppied');
+        } catch {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await rename();
