559493bc24719d9ae10dda42b8faccada7706cf0
Iryna Yehorova <interest4me@gmail.com>
2024-01-26 15:44:51 +0200

    fix: correct solution of create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,10 +1,18 @@
 import { writeFile } from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const create = async () => {
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const file = join(__dirname, "fresh.txt");
     const data = "I am fresh and young";
-    const file = "fresh.txt";
-    writeFile(file, data, {flag: 'wx'})
-    .then(() => console.log(` => file ${file} was created`))
-    .catch(() => console.log('FS operation failed'))
+
+    try{
+        await writeFile(file, data, {flag: 'wx'})
+        console.log(` => File was created`);
+    }catch{
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
