3b85f0d0e526c1f9d8190dddd9f6c0210318508f
Iryna Yehorova <interest4me@gmail.com>
2024-01-26 15:34:46 +0200

    feat: add solution to copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,4 +1,18 @@
+import { mkdir, readdir, copyFile }  from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 const copy = async () => {
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const srcFolder = join(__dirname, 'files/');
+    const projectFolder = join(__dirname, 'files_copy');
+    try{
+        await mkdir(projectFolder);
+        const files = await readdir(srcFolder);
+        files.map((file) => copyFile(join(srcFolder, file), join(projectFolder, file)));
+        console.log('Files were successfully coppied')
+    } catch {
+        throw new Error('FS operation failed')
+    }
     
 };
 
