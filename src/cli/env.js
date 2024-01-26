7e29f646b3b012e0f369991d437a0864face8656
Iryna Yehorova <interest4me@gmail.com>
2024-01-26 18:13:05 +0200

    feat: add solution to env file

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
+import { env } from "node:process";
 const parseEnv = () => {
-    // Write your code here 
+    const rssVars = Object.entries(env).filter(([key, _]) => key.startsWith("RSS"))
+    const rssVarsString = rssVars.map(([key, value]) => `${key}=${value}`).join('; ');
+    console.log(rssVarsString);
 };
 
 parseEnv();
