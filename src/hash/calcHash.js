341dafd6d8438f699424a576d9c705c8cbd1b318
Iryna Yehorova <interest4me@gmail.com>
2024-01-26 19:55:06 +0200

    feat: add solution to hash file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from 'node:crypto';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readFile } from 'node:fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const fileToHash = join(__dirname, 'files/fileToCalculateHashFor.txt');
+    try{
+       const text = await readFile(fileToHash);
+       const hash = createHash('sha256').update(text).digest('hex');
+       console.log(hash);
+    } catch {
+        throw Error("Can't read file");
+    }
+  
 };
 
 await calculateHash();
