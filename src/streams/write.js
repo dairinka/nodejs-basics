0d23dfcc7f2e250dd84803000ba416e09f3dc420
Iryna Yehorova <interest4me@gmail.com>
2024-01-26 22:04:06 +0200

    feat: add solution to stream write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { pipeline } from 'node:stream/promises';
+import { stdin } from "node:process";
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import {createWriteStream} from "node:fs"
 const write = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const fileToWrite = join(__dirname, 'files/fileToWrite.txt');
+    console.log('>>> Hi! Now you can wtite some text below. \nAfter you finish, please press Ctrl + C. \nSee your result in streams/files/fileToWrite.txt <<<')
+    await pipeline(stdin, createWriteStream(fileToWrite));
 };
 
 await write();
