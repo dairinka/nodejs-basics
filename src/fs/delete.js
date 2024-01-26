8df5551631aebfd16444e888f477e9b46ac5c8c8
Iryna Yehorova <interest4me@gmail.com>
2024-01-26 17:10:19 +0200

    feat: add solution to delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { rm }  from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const remove = async () => {
-    // Write your code here 
+   const __dirname = dirname(fileURLToPath(import.meta.url));
+   const srcFolder = join(__dirname, 'files/');
+   const fileToDelete = join(srcFolder, 'fileToRemove.txt');
+
+   try{
+    await rm(fileToDelete);
+    console.log('File was successfuly removed');
+   } catch {
+     throw Error('FS operation failed');
+   }
+
 };
 
 await remove();
