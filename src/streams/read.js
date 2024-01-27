816c35a6496672a9d12da3605922bacbf709c55e
Iryna Yehorova <interest4me@gmail.com>
2024-01-27 23:31:07 +0200

    fix: add line break to stream read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -3,15 +3,21 @@ import { stdout } from "node:process";
 import { createReadStream } from 'node:fs';
 import { join, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { Transform } from 'node:stream';
 
 const read = async () => {
     const __dirname = dirname(fileURLToPath(import.meta.url));
     const fileToRead = join(__dirname, 'files/fileToRead.txt');
    
+   const addLineBreak = new Transform({
+        transform(chank, encoding, callback){
+            callback(null, String(chank).replace(/(\w)$/, `$1\n` ));
+        }
+    })
     try {
-        await pipeline(createReadStream(fileToRead), stdout);
+        await pipeline(createReadStream(fileToRead), addLineBreak, stdout);
      } catch {
-        throw Error("Something went wrong");
+        throw Error("Oops! Something went wrong. Please contact to developer :)");
     }
     
 };
