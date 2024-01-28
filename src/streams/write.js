9e26ff88ca1fc5b3b61f08382c75659974ad6879
Iryna Yehorova <interest4me@gmail.com>
2024-01-29 01:14:46 +0200

    refactor: correct letter

--- src/streams/write.js
+++ src/streams/write.js
@@ -6,7 +6,7 @@ import {createWriteStream} from "node:fs"
 const write = async () => {
     const __dirname = dirname(fileURLToPath(import.meta.url));
     const fileToWrite = join(__dirname, 'files/fileToWrite.txt');
-    console.log('>>> Hi! Now you can wtite some text below. \nAfter you finish, please press Ctrl + C. \nSee your result in streams/files/fileToWrite.txt <<<')
+    console.log('>>> Hi! Now you can write some text below. \nAfter you finish, please press Ctrl + C. \nSee your result in streams/files/fileToWrite.txt <<<')
     await pipeline(stdin, createWriteStream(fileToWrite));
 };
 
