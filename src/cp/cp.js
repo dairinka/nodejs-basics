f1b84012fb3e494b44f1f352d245d331f239d503
Iryna Yehorova <interest4me@gmail.com>
2024-01-29 00:39:02 +0200

    feat: add solution to cp file

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { fork } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const childProcessFile = join(__dirname, "files", "script.js");
+    console.log(">> Hi there. I just remind: you need write some stuff in console, when you get bored just write CLOSE <<")
+    console.log(">> Thank you for check my work <<\n")
+    const childProc = fork(childProcessFile, args, { stdio: [0, 1, 2, 'ipc'] })
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
