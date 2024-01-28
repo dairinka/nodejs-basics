742c3f67ce56ff6bd99847915280cf991dae6c02
Iryna Yehorova <interest4me@gmail.com>
2024-01-29 00:52:54 +0200

    fix: change location of created file

--- src/fs/create.js
+++ src/fs/create.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'node:url';
 
 const create = async () => {
     const __dirname = dirname(fileURLToPath(import.meta.url));
-    const file = join(__dirname, "fresh.txt");
+    const file = join(__dirname, "files", "fresh.txt");
     const data = "I am fresh and young";
 
     try{
