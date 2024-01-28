55d1dc3c216f439f759bd383ae71bda96e85d406
Iryna Yehorova <interest4me@gmail.com>
2024-01-28 20:53:02 +0200

    feat: add solution to worker threads section

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,43 @@
+import { cpus } from "node:os"
+import { Worker, MessageChannel } from 'node:worker_threads';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const performCalculations = async () => {
-    // Write your code here
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const workerFile = join(__dirname, 'worker.js');
+
+    const cores = cpus();
+    const workerResults = [];
+
+    const createWorker = (n) => {
+        return new Promise((res, rej) => {
+            const worker = new Worker(workerFile);
+            worker.postMessage(n + 10);
+            worker.on("message", res);
+            worker.on("error", rej)
+        });
+
+    }
+
+    for (let i = 0; i < cores.length; i++) {
+        const workerResult = {
+            'status': '',
+            'data': 0
+        };
+
+        try {
+            workerResult.data = await createWorker(i);
+            workerResult.status = 'resolved';
+        } catch {
+            workerResult.data = null;
+            workerResult.status = 'error';
+        }
+
+        workerResults.push(workerResult);
+    }
+    console.log(workerResults);
+    process.exit(0);
 };
 
 await performCalculations();
